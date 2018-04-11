d5f080b9f5675b15f05950f16904510e8f54dbc9
jbysiak <37413896+jbysiak@users.noreply.github.com>
2018-04-11 22:31:02 +0200

    Add files via upload

---
+++ SimTask.java
@@ -0,0 +1,4 @@
+import java.util.TimerTask;
+
+public class SimTask extends TimerTask {
+}
---
+++ Vector2D.java
@@ -0,0 +1,23 @@
+public class Vector2D {
+    public int x, y;
+    public Vector2D()
+    {
+        x=0;
+        y=0;
+    }
+
+    public Vector2D(int X, int Y)
+    {
+        x=X;
+        y=Y;
+    }
+
+
+    public double Length()
+    {
+     //   double l=sqrt(x*x + y*y);
+     //   return l;
+    }
+
+    public static void main(String[] args) {}
+}
